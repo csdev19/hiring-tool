27e691a21f73c887c0438deaf4186bfbd1570eaf
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-01-08 11:45:50 -0500

    fix: test wrangler

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,18 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import alchemy from "alchemy/cloudflare/tanstack-start";
+// import alchemy from "alchemy/cloudflare/tanstack-start";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact(), alchemy()],
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    // alchemy()
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+  ],
   server: {
     port: 3001,
   },
