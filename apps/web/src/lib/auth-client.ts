23bd08592be91be7ea586895381b84d032f9f7ac
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-01-09 11:09:17 -0500

    fix: remove tanstack start cookies

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { createAuthClient } from "better-auth/react";
-import { tanstackStartCookies } from "better-auth/tanstack-start";
 
-console.log("import.meta.env.VITE_SERVER_URL", import.meta.env.VITE_SERVER_URL);
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_SERVER_URL,
-  plugins: [tanstackStartCookies()],
+  // plugins: [tanstackStartCookies()],
 });
