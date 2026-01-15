5c6866c0e79378110bda0ee3020f106654389def
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-01-14 22:18:21 -0500

    fix: trying to fix 14 jan

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,20 +3,26 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import alchemy from "alchemy/cloudflare/tanstack-start";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact(), alchemy()],
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tanstackStart(),
+    viteReact(),
+  ],
   server: {
     port: 3001,
   },
-  optimizeDeps: {
-    force: true,
-  },
-  css: {
-    devSourcemap: true,
-  },
-  build: {
-    cssCodeSplit: false,
-  },
+  // optimizeDeps: {
+  //   force: true,
+  // },
+  // css: {
+  //   devSourcemap: true,
+  // },
+  // build: {
+  //   cssCodeSplit: false,
+  // },
 });
