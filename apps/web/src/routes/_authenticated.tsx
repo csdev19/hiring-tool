34b91fb6f81b6abf2d4067d7cab6cbc7eae3da06
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-01-15 11:33:19 -0500

    fix: logging

--- apps/web/src/routes/_authenticated.tsx
+++ apps/web/src/routes/_authenticated.tsx
@@ -2,16 +2,33 @@ import { authClient } from "@/lib/auth-client";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
+import { createAuthClient } from "better-auth/react";
 
 // Get current user
 export const getCurrentUserFn = createServerFn({ method: "GET" }).handler(async () => {
   const headers = await getRequestHeaders();
+  console.log("headers ->", headers);
+
+  console.log("import.meta.env.VITE_SERVER_URL", import.meta.env.VITE_SERVER_URL);
+  console.log("process.env.VITE_SERVER_URL", process.env.VITE_SERVER_URL);
 
   const { data, error } = await authClient.getSession({
     fetchOptions: {
       headers: headers,
     },
   });
+
+  const authClient2 = createAuthClient({
+    baseURL: "https://hiring-tool-api.cristiansotomayor-dev.workers.dev",
+  });
+
+  const data2 = await authClient2.getSession({
+    fetchOptions: {
+      headers: headers,
+    },
+  });
+
+  console.log("data2 ->", data2);
 
   if (error) {
     console.log("error", error);
@@ -37,8 +54,6 @@ export const Route = createFileRoute("/_authenticated")({
       // throw redirect({ to: "/auth/login" });
     }
 
-    // TODO: context.session should be typed from the middleware
-    // For now, the middleware handles the redirect if not authenticated
     return { session };
   },
 });
