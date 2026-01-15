4cd69a1dc9e789e86fbb72cbdc458cae0d40aac3
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-01-15 12:04:26 -0500

    fix: 2

--- apps/web/src/routes/_authenticated.tsx
+++ apps/web/src/routes/_authenticated.tsx
@@ -5,7 +5,7 @@ import { getRequestHeaders } from "@tanstack/react-start/server";
 import { createAuthClient } from "better-auth/react";
 
 // Get current user
-export const getCurrentUserFn = createServerFn({ method: "GET" }).handler(async () => {
+export const getCurrentUserFn = createServerFn({ method: "GET" }).handler(async ({ context }) => {
   const headers = await getRequestHeaders();
   console.log("headers ->", headers);
 
@@ -16,27 +16,15 @@ export const getCurrentUserFn = createServerFn({ method: "GET" }).handler(async 
   const cookieHeader = headers.get("cookie");
   console.log("cookieHeader ->", cookieHeader);
 
-  const { data, error } = await authClient.getSession({
+  const sessionData = await authClient.getSession({
     fetchOptions: {
       headers: headers,
+      credentials: "include",
     },
   });
 
-  const authClient2 = createAuthClient({
-    baseURL: "https://hiring-tool-api.cristiansotomayor-dev.workers.dev",
-  });
-
-  const data2 = await authClient2.getSession({
-    fetchOptions: {
-      headers: {
-        ...headers,
-        cookie: cookieHeader,
-        "content-type": "application/json",
-      },
-    },
-  });
-
-  console.log("data2 ->", data2);
+  console.log("sessionData ->", sessionData);
+  const { data, error } = sessionData;
 
   if (error) {
     console.log("error", error);
