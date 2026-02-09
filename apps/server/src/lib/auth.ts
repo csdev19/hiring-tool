3e2fd06b28b74f32539cb9d656a691ad82e60b0a
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-02-09 08:56:05 -0500

    remove this 8

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { baseConfig, getCustomSession } from "@interviews-tool/infra-auth";
 import { betterAuth } from "better-auth";
 import { customSession } from "better-auth/plugins";
-import { expo } from "@better-auth/expo";
+// import { expo } from "@better-auth/expo";
 import { env } from "cloudflare:workers";
 
 // export const auth: ReturnType<typeof betterAuth> = betterAuth({
@@ -9,5 +9,6 @@ export const auth = betterAuth({
   ...baseConfig,
   baseURL: env.BETTER_AUTH_URL,
   trustedOrigins: [env.CORS_ORIGIN, "exp://", "mobile://", "exp://*"],
-  plugins: [...(baseConfig.plugins ?? []), customSession(getCustomSession, baseConfig), expo()],
+  // plugins: [...(baseConfig.plugins ?? []), customSession(getCustomSession, baseConfig), expo()],
+  plugins: [...(baseConfig.plugins ?? []), customSession(getCustomSession, baseConfig)],
 });
