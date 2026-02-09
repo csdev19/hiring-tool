473cb59d50c298101f3f61b00d8de1c7f251ed09
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-02-09 14:43:40 -0500

    change 14

--- apps/web/src/routes/api/auth/$.ts
+++ apps/web/src/routes/api/auth/$.ts
@@ -5,18 +5,23 @@ export const Route = createFileRoute("/api/auth/$")({
   server: {
     handlers: {
       GET: async ({ request }) => {
+        console.log("get request =======> ");
         return auth.handler(request);
       },
       POST: async ({ request }) => {
+        console.log("post request =======> ");
         return auth.handler(request);
       },
       PUT: async ({ request }) => {
+        console.log("put request =======> ");
         return auth.handler(request);
       },
       PATCH: async ({ request }) => {
+        console.log("patch request =======> ");
         return auth.handler(request);
       },
       DELETE: async ({ request }) => {
+        console.log("delete request =======> ");
         return auth.handler(request);
       },
     },
