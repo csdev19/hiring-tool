80a659193c614348f9651dbf26c92018c15c9c65
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-02-09 08:38:55 -0500

    remove this 6

--- apps/web/src/lib/auth/auth-server.ts
+++ apps/web/src/lib/auth/auth-server.ts
@@ -1,10 +1,10 @@
 import { betterAuth } from "better-auth";
-// import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { baseConfig } from "@interviews-tool/infra-auth";
 
 export const auth = betterAuth({
   ...baseConfig,
   baseURL: process.env.BETTER_AUTH_URL,
-  // plugins: [...(baseConfig.plugins ?? []), tanstackStartCookies()],
-  plugins: [...(baseConfig.plugins ?? [])],
+  plugins: [...(baseConfig.plugins ?? []), tanstackStartCookies()],
+  // plugins: [...(baseConfig.plugins ?? [])],
 });
