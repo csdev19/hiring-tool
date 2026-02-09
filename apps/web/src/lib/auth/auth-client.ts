5e4a98e186e67f13ad14d59cdef472b1b0ee4690
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-02-09 16:28:19 -0500

    test

--- apps/web/src/lib/auth/auth-client.ts
+++ apps/web/src/lib/auth/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  // baseURL: import.meta.env.VITE_SERVER_URL,
+  // basePath: "/api/auth",
   fetchOptions: {
     credentials: "include",
   },
