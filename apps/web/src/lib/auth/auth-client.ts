6ce71dd331a129f7266090793e0504cd0085ff53
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-02-09 10:46:06 -0500

    change 13

--- apps/web/src/lib/auth/auth-client.ts
+++ apps/web/src/lib/auth/auth-client.ts
@@ -2,6 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   // baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: "https://hiring-tool-web.cristiansotomayor-dev.workers.dev",
   fetchOptions: {
     credentials: "include",
   },
