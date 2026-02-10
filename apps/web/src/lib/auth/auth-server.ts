3ce56c66f70d15492ced3838ffe22fd5ce284cda
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2026-02-09 22:02:28 -0500

    using our own origin

--- apps/web/src/lib/auth/auth-server.ts
+++ apps/web/src/lib/auth/auth-server.ts
@@ -5,5 +5,6 @@ import { baseConfig } from "@interviews-tool/infra-auth";
 export const auth = betterAuth({
   ...baseConfig,
   // trustedOrigins: ["*"],
+  trustedOrigins: ["https://hiring-tool-web.cristiansotomayor-dev.workers.dev"],
   plugins: [...(baseConfig.plugins ?? []), tanstackStartCookies()],
 });
